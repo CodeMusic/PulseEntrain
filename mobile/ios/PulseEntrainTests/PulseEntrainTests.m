3fbe8bc0ba61e2463c179c5d64eae03f36bea732
CodeMusic <themusicofthecode@gmail.com>
2026-06-14 17:28:49 -0400

    initial mobile fork refactor
    start refactoring to support pulse entrainment.

--- mobile/ios/PulseEntrainTests/PulseEntrainTests.m
+++ mobile/ios/PulseEntrainTests/PulseEntrainTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface PulseLibreTests : XCTestCase
+@interface PulseEntrainTests : XCTestCase
 
 @end
 
-@implementation PulseLibreTests
+@implementation PulseEntrainTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
